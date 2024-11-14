0512dc3d07e14b7b5c20ce65b22046433c113005
Eiman Salek <eimanabde04@gmail.com>
2024-11-14 16:16:34 +0100

    En funcionamiento

--- src/main/java/com/eiman/ejp/ProductoApplication.java
+++ src/main/java/com/eiman/ejp/ProductoApplication.java
@@ -5,29 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
  * Clase principal para iniciar la aplicacion.
  */
 public class ProductoApplication extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Cargar el archivo FXML principal
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/producto.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        // Cargar el archivo FXML principal
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml_2/Producto.fxml"));
 
-            // Crear la escena y configurarla
-            Scene scene = new Scene(loader.load());
+        // Crear la escena y configurarla
+        Scene scene = new Scene(loader.load());
 
-            // Configurar y mostrar la ventana principal
-            primaryStage.setTitle("Lista de Productos");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error al cargar producto.fxml", e);
-        }
+        // Configurar y mostrar la ventana principal
+        primaryStage.setTitle("Lista de Productos");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     /**
--- src/main/java/com/eiman/ejp/ProductoController.java
+++ src/main/java/com/eiman/ejp/ProductoController.java
@@ -1,38 +1,42 @@
 package com.eiman.ejp;
 
-import com.eiman.ejo.ProductoApplication; // Importar desde el JAR
+import com.eiman.ejo.Producto; // Importar desde el JAR
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Controlador para la interfaz principal definida en producto.fxml.
  */
-public class ProductoController {
+public class ProductoController implements Initializable {
 
     @FXML
-    private ProductoApplication panProducto;
+    private Producto panProducto;
 
     @FXML
-    private ProductoApplication manzanaProducto;
+    private Producto manzanaProducto;
 
     @FXML
-    private ProductoApplication lecheProducto;
+    private Producto lecheProducto;
 
-    @FXML
-    public void initialize() {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         // Configurar el producto "Pan"
         panProducto.setName("Pan");
         panProducto.setStock(10);
-        panProducto.setImage(new Image(getClass().getResource("/images/pan.png").toExternalForm()));
+        panProducto.setImage(new Image(getClass().getResource("/images_2/pan.png").toExternalForm()));
 
         // Configurar el producto "Manzana"
         manzanaProducto.setName("Manzana");
         manzanaProducto.setStock(50);
-        manzanaProducto.setImage(new Image(getClass().getResource("/images/manzana.png").toExternalForm()));
+        manzanaProducto.setImage(new Image(getClass().getResource("/images_2/manzana.png").toExternalForm()));
 
         // Configurar el producto "Leche"
         lecheProducto.setName("Leche");
         lecheProducto.setStock(200);
-        lecheProducto.setImage(new Image(getClass().getResource("/images/leche.png").toExternalForm()));
+        lecheProducto.setImage(new Image(getClass().getResource("/images_2/leche.png").toExternalForm()));
     }
 }
