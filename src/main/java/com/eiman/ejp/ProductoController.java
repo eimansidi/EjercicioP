0f0883d82c1fed8a68d84e4054dbe6447d47d6ac
Eiman Salek <eimanabde04@gmail.com>
2024-11-14 14:11:48 +0100

    Controller creado

--- src/main/java/com/eiman/ejp/ProductoController.java
+++ src/main/java/com/eiman/ejp/ProductoController.java
@@ -1,14 +1,38 @@
 package com.eiman.ejp;
 
+import com.eiman.ejo.ProductoApplication; // Importar desde el JAR
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
+/**
+ * Controlador para la interfaz principal definida en producto.fxml.
+ */
 public class ProductoController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ProductoApplication panProducto;
+
+    @FXML
+    private ProductoApplication manzanaProducto;
+
+    @FXML
+    private ProductoApplication lecheProducto;
+
+    @FXML
+    public void initialize() {
+        // Configurar el producto "Pan"
+        panProducto.setName("Pan");
+        panProducto.setStock(10);
+        panProducto.setImage(new Image(getClass().getResource("/images/pan.png").toExternalForm()));
+
+        // Configurar el producto "Manzana"
+        manzanaProducto.setName("Manzana");
+        manzanaProducto.setStock(50);
+        manzanaProducto.setImage(new Image(getClass().getResource("/images/manzana.png").toExternalForm()));
+
+        // Configurar el producto "Leche"
+        lecheProducto.setName("Leche");
+        lecheProducto.setStock(200);
+        lecheProducto.setImage(new Image(getClass().getResource("/images/leche.png").toExternalForm()));
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.eiman.ejp {
     requires com.eiman.ejo;
     requires javafx.controls;
+    requires javafx.fxml;
 
 
     opens com.eiman.ejp to javafx.fxml;
